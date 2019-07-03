ac729ec41ab278b0c93036f8d69c32602a92d2a8
khuynh <hkennyv@gmail.com>
2019-07-03 11:37:45 -0700

    add json support for staticbox and messagebox

--- src/StaticBox.js
+++ src/StaticBox.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import { Input, TextArea } from 'semantic-ui-react';
 
+function renderMessage(message) {
+    try {
+        return JSON.stringify(JSON.parse(message), null, 2);
+    } catch (err) {
+        return message;
+    }
+}
+
 const StaticBox = props => {
     const { message, topic } = props;
     const { onChangeInput1, onSubmitInput1 } = props;
@@ -11,7 +19,7 @@ const StaticBox = props => {
             <form onSubmit={onSubmitInput1}>
                 <Input fluid size="mini" label="Topic:" onChange={onChangeInput1} />
             </form>
-            <TextArea rows={5} value={message} />
+            <TextArea rows={5} value={renderMessage(message)} />
         </div>
     );
 }
